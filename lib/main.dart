4703555d49682db0ad4a2e7edfd8e7b73b4b1d4c
axis.1410 <adityasingh14102001@gmail.com>
2023-01-29 15:19:19 +0530

    Connected firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:voting_app/screens/landing_page.dart';
 
-void main() {
+import './screens/landing_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,7 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('ElectChain'),
         actions: [],
       ),
-      drawer: ProfileDrawer(),
+      drawer: const ProfileDrawer(),
       body: SingleChildScrollView(
         child: Container(
           margin: const EdgeInsets.fromLTRB(0.0, 12.0, 0.0, 10.0),
@@ -30,12 +30,9 @@ class _HomeScreenState extends State<HomeScreen> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               // SizedBox(height: 30.0),
-              Text(
+              const Text(
                 "ENTER A VOTE CODE",
-                style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.indigo),
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold, color: Colors.indigo),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -46,26 +43,24 @@ class _HomeScreenState extends State<HomeScreen> {
                       height: 50.0,
                       margin: const EdgeInsets.only(top: 20.0),
                       decoration: BoxDecoration(
-                          color: Colors.grey[300],
-                          borderRadius: BorderRadius.circular(18.0)),
+                          color: Colors.grey[300], borderRadius: BorderRadius.circular(18.0)),
                       child: Form(
                         key: GlobalKey<FormState>(),
                         child: TextFormField(
                           controller: _electionAccessCodeController,
                           keyboardType: TextInputType.text,
-                          style: TextStyle(
+                          style: const TextStyle(
                             fontSize: 22.0,
                             color: Colors.indigo,
                             fontWeight: FontWeight.bold,
                           ),
                           decoration: InputDecoration(
                             fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(18.0)),
+                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(18.0)),
                             hintText: "Enter the election code",
-                            hintStyle: TextStyle(
-                                fontSize: 16.0, fontWeight: FontWeight.normal),
-                            prefixIcon: Icon(
+                            hintStyle:
+                                const TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal),
+                            prefixIcon: const Icon(
                               Icons.lock,
                             ),
                           ),
@@ -74,26 +69,28 @@ class _HomeScreenState extends State<HomeScreen> {
                   Container(
                     margin: const EdgeInsets.only(top: 20.0, left: 5.0),
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(18.0),
-                        gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [Colors.indigo, Colors.blue])),
+                      borderRadius: BorderRadius.circular(18.0),
+                      gradient: const LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [Colors.indigo, Colors.blue],
+                      ),
+                    ),
                     child: TextButton.icon(
                         onPressed: (() => {}),
                         //Database Code to be addded
-                        icon: Icon(
+                        icon: const Icon(
                           Icons.check_circle,
                           color: Colors.white,
                         ),
-                        label: Text(
+                        label: const Text(
                           "Validate",
                           style: TextStyle(color: Colors.white, fontSize: 20.0),
                         )),
                   )
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 40.0,
               ),
               Row(
@@ -101,19 +98,17 @@ class _HomeScreenState extends State<HomeScreen> {
                 children: [
                   GestureDetector(
                     onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: ((context) => NewVote())));
+                      Navigator.push(
+                          context, MaterialPageRoute(builder: ((context) => const NewVote())));
                     },
-                    child: ActionBox(
+                    child: const ActionBox(
                       action: "Create Election",
                       description: "Create a new vote",
                       image: Icons.how_to_vote,
                     ),
                   ),
-                  ActionBox(
-                      action: "Poll",
-                      description: "Create a new poll",
-                      image: Icons.poll),
+                  const ActionBox(
+                      action: "Poll", description: "Create a new poll", image: Icons.poll),
                 ],
               ),
 
@@ -125,17 +120,15 @@ class _HomeScreenState extends State<HomeScreen> {
                         context,
                         MaterialPageRoute(
                             builder: ((context) =>
-                                UserElections()))), //Get.to(UserElections()),
-                    child: ActionBox(
+                                const UserElections()))), //Get.to(UserElections()),
+                    child: const ActionBox(
                       action: "My Elections",
                       description: "Create a new vote",
                       image: Icons.ballot,
                     ),
                   ),
-                  ActionBox(
-                      action: "FAQ",
-                      description: "Create a new poll",
-                      image: Icons.description),
+                  const ActionBox(
+                      action: "FAQ", description: "Create a new poll", image: Icons.description),
                 ],
               ),
             ],
