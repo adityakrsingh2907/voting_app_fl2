94e80cd56405dfdee6d7a64e69b66e8b3ca434be
axis.1410 <adityasingh14102001@gmail.com>
2023-02-04 18:00:17 +0530

    Edited Sign In Page

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -15,24 +15,8 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
-  final _formKey = GlobalKey<FormState>();
+
   bool isLoading = false;
-
-  String email = '';
-  String password = '';
-  String error = '';
-
-  void signInWithEmailAndPassword(String email, String password) async {
-    if (_formKey.currentState!.validate()) {
-      setState(() => isLoading = true);
-      dynamic result = await _auth.signInWithEmailAndPassword(email, password);
-
-      if (result == null) {
-        setState(() => isLoading = false);
-        setState(() => error = 'Could not sign in with those credentials');
-      }
-    }
-  }
 
   void signInWithGoogle(BuildContext context) {
     _auth.signInWithGoogle(context);
@@ -41,39 +25,41 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Constants.backgroundColor1,
       appBar: AppBar(
         title: const Text('Sign In'),
         backgroundColor: Constants.appBarColor,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: ElevatedButton.icon(
-            onPressed: () => signInWithGoogle(context),
-            icon: Image.asset(
-              Constants.googlePath,
-              width: 35,
-            ),
-            style: ElevatedButton.styleFrom(
-              elevation: 5,
-              backgroundColor: Constants.gradientColor1,
-              minimumSize: const Size(double.infinity, 50),
-              // shape: const StadiumBorder(),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+      body: isLoading
+          ? const Loading()
+          : Center(
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: ElevatedButton.icon(
+                  onPressed: () => signInWithGoogle(context),
+                  icon: Image.asset(
+                    Constants.googlePath,
+                    width: 35,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    backgroundColor: Constants.gradientColor1,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  label: const Text(
+                    'Continue with google',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.7,
+                    ),
+                  ),
+                ),
               ),
             ),
-            label: const Text(
-              // style the text to look sexy
-              'Continue with google',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
